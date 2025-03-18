be416bc0f28d24c4d24ad30ec6cb0d46cf110df9
Shohrukh Koyirov <shohrukhkoyirov@gmail.com>
2025-03-18 13:45:38 +0100

    start stop timeout implemented

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,12 +1,14 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 function TimerChallenge({ title, targetTime }) {
+
+    const timer = useRef();
 
     const [timerStarted, setTimerStarted] = useState(false);
     const [timerExpired, setTimerExpired] = useState(false);
 
     const handleStart = () => {
-        setTimeout(() => {
+        timer.current = setTimeout(() => {
             setTimerExpired(true);
         }, targetTime * 1000);
 
@@ -14,11 +16,8 @@ function TimerChallenge({ title, targetTime }) {
     }
 
     const handleStop = () => {
-        setTimeout(() => {
-            setTimerExpired(true);
-        }, targetTime * 1000);
-
-        setTimerStarted(true);
+        clearTimeout(timer.current)
+        setTimerStarted(false);
     }
 
     return (
@@ -29,7 +28,7 @@ function TimerChallenge({ title, targetTime }) {
                 {targetTime} second{targetTime > 1 ? 's' : ''}
             </p>
             <p>
-                <button onClick={handleStart}>
+                <button onClick={timerStarted ? handleStop : handleStart}>
                     {timerStarted ? 'Stop Challenge' : 'Start Challenge'}
                 </button>
             </p>
